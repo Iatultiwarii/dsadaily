f691d88a4e2ae608ea96fa4cd3e24c465a7879d1
ATUL TIWARI <119276854+Iatultiwarii@users.noreply.github.com>
2025-03-01 21:50:33 +0530

    Create arrayoperation0301.js

---
+++ arrayoperation0301.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var applyOperations = function(nums) {
+   
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums[i] *= 2;
+            nums[i + 1] = 0;
+        }
+    }
+    let j = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+
+    while (j < nums.length) {
+        nums[j] = 0;
+        j++;
+    }
+
+    return nums;
+};
